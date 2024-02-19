a264416625feede46ac209664849e0d344ade373
KevinKaris <kevinkarish001@gmail.com>
2024-02-19 18:03:34 +0300

    update sql file

--- timesheet (1).sql
+++ timesheet (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 05, 2024 at 12:04 PM
+-- Generation Time: Feb 09, 2024 at 11:28 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,8 +40,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `username`, `email`, `department`, `password`) VALUES
-(10, 'Kevin', 'kevinkarish001@gmail.com', '100', '12345678'),
-(11, 'Kevin_HR', 'kevinkarish983@gmail.com', '102', '12345678');
+(12, 'Emmanuel', 'emmanuelngeno23@gmail.com', '001', '31713171'),
+(13, 'Manuu', '18670@student.embuni.ac.ke', '002', '11111111');
 
 -- --------------------------------------------------------
 
@@ -60,9 +60,11 @@ CREATE TABLE `branch` (
 --
 
 INSERT INTO `branch` (`id`, `branch_number`, `branch_name`) VALUES
-(10, 'b334', 'Molo'),
-(14, 'b001', 'Ngong'),
-(15, 'c100', 'Nairobi');
+(16, '1', 'University of Embu'),
+(17, '2', 'Embu college '),
+(18, '3', 'KSG'),
+(19, '4', 'Kayole'),
+(20, '5', 'Bagik');
 
 -- --------------------------------------------------------
 
@@ -81,14 +83,12 @@ CREATE TABLE `department` (
 --
 
 INSERT INTO `department` (`id`, `department_number`, `department_name`) VALUES
-(9, '100', 'ICT'),
-(10, '101', 'Procurement '),
-(11, '102', 'HR'),
-(12, '103', 'Finance'),
-(13, '104', 'Security '),
-(14, '105', 'Sales and marketing'),
-(15, '106', 'Management'),
-(16, '107', 'Data management');
+(17, '001', 'ICT'),
+(18, '002', 'Procurement'),
+(19, '003', 'Finance'),
+(20, '004', 'SESS'),
+(21, '005', 'Economics'),
+(22, '006', 'Education');
 
 -- --------------------------------------------------------
 
@@ -106,6 +106,7 @@ CREATE TABLE `employee` (
   `under_leave` text NOT NULL,
   `department` text NOT NULL,
   `phone` int(10) NOT NULL,
+  `last_login` timestamp NULL DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -113,8 +114,9 @@ CREATE TABLE `employee` (
 -- Dumping data for table `employee`
 --
 
-INSERT INTO `employee` (`employee_id`, `username`, `fname`, `lname`, `email`, `approval`, `under_leave`, `department`, `phone`, `password`) VALUES
-(33, 'Kevin', 'Kevin', 'Kariuki', 'kevinkarish983@gmail.com', 'Approved', 'No', '100', 746182038, '$2y$10$UwxmpnlFz9usyqcIL11EmOTVHe7eKddCgLza84y6YS1SFwxB04cxO');
+INSERT INTO `employee` (`employee_id`, `username`, `fname`, `lname`, `email`, `approval`, `under_leave`, `department`, `phone`, `last_login`, `password`) VALUES
+(35, 'Emmanuel', 'Emmanuel', 'Ngeno', 'emmanuelngeno23@gmail.com', 'Approved', 'No', '001', 719191921, '2024-02-09 09:33:02', '$2y$10$1jpzZb5RLzWt3qwLu35ioe1pBFcXq4X/ICT2FmF4WTexXCK5thFRe'),
+(36, 'Collo', 'Collins ', 'Kiplangat', '27383@student.embuni.ac.ke', 'Approved', 'Yes', '002', 701574737, '2024-02-08 11:01:44', '$2y$10$0xn0XhDIoWn.9//QnVPGH.vtd5BXVimSw41dWOgBg5qIuRePS0mlu');
 
 -- --------------------------------------------------------
 
@@ -137,8 +139,10 @@ CREATE TABLE `leave_request` (
 --
 
 INSERT INTO `leave_request` (`id`, `employee_id`, `from_date`, `to_when`, `reason`, `accepted`, `date`) VALUES
-(3, 33, '2024-02-01', '2024-02-10', '<p>visit my family</p>', 'Yes', '2024-02-01 08:03:01'),
-(4, 33, '2024-02-10', '2024-02-17', '<p>skdncksjcn sciuhciu siuchsdiu&nbsp;</p>', 'No', '2024-02-01 10:30:28');
+(5, 34, '2024-02-05', '2024-02-08', '<p>We are now done&nbsp; with the attachment you can allow me to quite</p>', 'Yes', '2024-02-05 14:43:11'),
+(6, 35, '2024-02-06', '2024-02-14', '<p>This is just to confirm if you can allow me to leave because am done with the attachment&nbsp;</p>', 'Yes', '2024-02-05 16:44:54'),
+(7, 35, '2024-02-06', '2024-02-07', '<p>We are now heading to the completion of the attachement session, I\'m now ready to quit the company.</p>', 'Yes', '2024-02-06 13:26:12'),
+(8, 36, '2024-02-10', '2024-02-22', '<p>Am almost done with my attachment. Days I was given by the Organization is almost collapsing&nbsp;</p>', 'Yes', '2024-02-08 11:16:51');
 
 -- --------------------------------------------------------
 
@@ -165,11 +169,19 @@ CREATE TABLE `message` (
 --
 
 INSERT INTO `message` (`id`, `employee_id`, `admin_id`, `message_from`, `message_to`, `parent_message_id`, `department`, `content`, `message_read`, `admin_read`, `date`) VALUES
-(10, 33, NULL, 33, NULL, NULL, '100', 'test message', 'Yes', 'Yes', '2024-02-05 07:26:40'),
-(11, 33, 10, 10, 33, 10, '100', 'okay, how is you?', 'Yes', 'Yes', '2024-02-05 07:27:24'),
-(12, 33, 10, 33, 10, 10, '100', 'alright', 'Yes', 'Yes', '2024-02-05 07:36:08'),
-(13, 33, 10, 10, 33, 10, '100', 'okay', 'Yes', 'Yes', '2024-02-05 08:08:20'),
-(14, 33, NULL, 33, NULL, NULL, '100', 'thank you', 'Yes', 'Yes', '2024-02-05 09:04:31');
+(16, 35, NULL, 35, NULL, NULL, '1', 'hey there', 'Yes', 'Yes', '2024-02-05 13:05:01'),
+(17, 35, 12, 12, 35, 16, '1', 'hi, how is you', 'Yes', 'Yes', '2024-02-05 13:05:46'),
+(18, 35, 12, 35, 12, 16, '1', 'Awesome, When are you coming to assess me?', 'Yes', 'Yes', '2024-02-05 15:18:23'),
+(19, 35, 12, 35, 12, 16, '1', 'were here all right\n', 'Yes', 'Yes', '2024-02-05 17:43:12'),
+(20, 35, 12, 12, 35, 16, '1', 'We are checking you,,\n', 'Yes', 'Yes', '2024-02-05 17:47:40'),
+(22, 35, NULL, 35, NULL, NULL, '1', 'Am coming to hold a meeting in ABH by 10am, Keep on working with the task we had.', 'Yes', 'Yes', '2024-02-06 13:23:02'),
+(23, 35, 12, 12, 35, 22, '1', 'Okay sir, am gonna keep bushing...', 'Yes', 'Yes', '2024-02-06 13:24:04'),
+(24, 35, 12, 35, 12, 22, '1', 'we are there with you\n', 'Yes', 'Yes', '2024-02-08 10:53:00'),
+(25, 35, 12, 12, 35, 22, '1', 'Ok am much pleased with your company ', 'Yes', 'Yes', '2024-02-08 10:53:43'),
+(26, 35, NULL, 35, NULL, NULL, '1', 'When are we going to watch our firm', 'Yes', 'Yes', '2024-02-08 10:54:53'),
+(27, 36, NULL, 36, NULL, NULL, '2', 'I have summitted my KRA file', 'Yes', 'Yes', '2024-02-08 11:05:47'),
+(28, 36, 13, 13, 36, 27, '2', 'Ok let me check', 'Yes', 'Yes', '2024-02-08 11:06:38'),
+(31, 36, 13, 36, 13, 27, '2', 'Why did you send me many reply of the same characters', 'Yes', 'Yes', '2024-02-08 11:11:36');
 
 -- --------------------------------------------------------
 
@@ -185,7 +197,7 @@ CREATE TABLE `report` (
   `place_of_work` text NOT NULL,
   `remarks` longtext NOT NULL,
   `picture` text NOT NULL,
-  `file` longblob NOT NULL,
+  `file` longblob DEFAULT NULL,
   `timeout` time NOT NULL DEFAULT current_timestamp(),
   `time_uploaded` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -195,7 +207,8 @@ CREATE TABLE `report` (
 --
 
 INSERT INTO `report` (`report_id`, `employee_id`, `date`, `time_in`, `place_of_work`, `remarks`, `picture`, `file`, `timeout`, `time_uploaded`) VALUES
-(47, 33, '2024-01-29', '11:39:00', 'Molo', '<p>KSNCK VKS K N On vndk vndkhvodvnk dhvkdfh vodhvkdfh</p>', '1706452814Screenshot 2024-01-19 194110.png', '', '17:40:14', '2024-01-28 14:40:14');
+(61, 35, '2024-02-06', '16:19:00', 'KSG', '<p>Here we are at the moment...</p>', '1707225629Screenshot_20240204-210423.png', NULL, '16:20:29', '2024-02-06 13:20:29'),
+(62, 35, '2024-02-08', '11:30:00', 'Kayole', '<p>We are almost done with the sem 2. We did the project as part of our learning.&nbsp;</p>', '1707389016remo.png', NULL, '13:43:36', '2024-02-08 10:43:36');
 
 --
 -- Indexes for dumped tables
@@ -252,43 +265,43 @@ ALTER TABLE `report`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `branch`
 --
 ALTER TABLE `branch`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `department`
 --
 ALTER TABLE `department`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `leave_request`
 --
 ALTER TABLE `leave_request`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `report`
 --
 ALTER TABLE `report`
-  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
